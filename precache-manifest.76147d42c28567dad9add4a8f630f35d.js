c4f3a48ac9be7a5a4ae5384ec2647c3067fd4974
shyam sundar <shyamsundar055@gmail.com>
2020-04-12 00:09:52 +0530

    Updates

---
+++ precache-manifest.76147d42c28567dad9add4a8f630f35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16382de1d3287310711ccc77e58710e",
+    "url": "/covid-india-app/index.html"
+  },
+  {
+    "revision": "1d3551e88fe21662618b",
+    "url": "/covid-india-app/static/css/main.51f41e2e.chunk.css"
+  },
+  {
+    "revision": "1b47c88bb4c9508c96c8",
+    "url": "/covid-india-app/static/js/2.a353e843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-india-app/static/js/2.a353e843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3551e88fe21662618b",
+    "url": "/covid-india-app/static/js/main.cb5654be.chunk.js"
+  },
+  {
+    "revision": "233081753ea981e6ebdb",
+    "url": "/covid-india-app/static/js/runtime-main.51b87872.js"
+  }
+]);
